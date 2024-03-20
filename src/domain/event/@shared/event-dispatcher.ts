937ef60038d6db343d6d272302aca212992f7c68
Lucian Tavares <lucian.sistemas.sc@gmail.com>
2024-03-20 10:49:54 -0300

    feat: :zap: Implement event customer

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -7,7 +7,7 @@ export default class EventDispatcher implements EventDispatcherInterface {
     private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
 
     get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
-    return this.eventHandlers;
+        return this.eventHandlers;
     }
 
     register(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
@@ -16,11 +16,11 @@ export default class EventDispatcher implements EventDispatcherInterface {
         }
         this.eventHandlers[eventName].push(eventHandler);
     }
-    
+
     unregister(eventName: string, eventHandler: EventHandlerInterface<EventInterface>): void {
-        if(this.eventHandlers[eventName]) {
+        if (this.eventHandlers[eventName]) {
             const index = this.eventHandlers[eventName].indexOf(eventHandler);
-            if(index !== -1) {
+            if (index !== -1) {
                 this.eventHandlers[eventName].splice(index, 1);
             }
         }
@@ -32,11 +32,11 @@ export default class EventDispatcher implements EventDispatcherInterface {
 
     notify(event: EventInterface): void {
         const eventName = event.constructor.name;
-        if(this.eventHandlers[eventName]) {
+        if (this.eventHandlers[eventName]) {
             this.eventHandlers[eventName].forEach((eventHandler) => {
                 eventHandler.handle(event);
             });
         }
     }
-    
+
 }
